46cdd34d448b100df4ca86f0955f2bf932f7605f
JiyanBlack <rglsm7655558@gmail.com>
2017-12-18 22:37:31 +0800

    loadData

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -1,17 +1,26 @@
-
-
-function startRun() {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+function initialize(geoArray, zoomLevel) {
+    var map = L.map('mapid').setView(geoArray, zoomLevel);
 
     L.tileLayer(
         'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
             maxZoom: 18,
             id: 'mapbox.streets'
-        }).addTo(mymap);
+        }).addTo(map);
 
 
     var controlEle = document.getElementsByClassName("leaflet-control-container")[0];
     controlEle.parentNode.removeChild(controlEle);
+
+    return map;
+}
+
+function startRun() {
+    var map = initialize([-31.986, 116.025], 11);
+    
+    L.geoJSON(wasub).addTo(map);
+
+    console.log(wasub);
+
 }
 
 
